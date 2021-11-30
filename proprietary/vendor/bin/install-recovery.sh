e2c1ad1edd4eff5c0f97b5156636fb3a918ff191
CloudedQuartz <ravenklawasd@gmail.com>
2021-11-30 21:24:51 +0530

    sweet: update blobs from MIUI V12.5.6.0 RKFEUXM
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- proprietary/vendor/bin/install-recovery.sh
+++ proprietary/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:a480d751110e5476fefdf4ed0ec642e94d075dd7; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:8c2a7c8d9a259d7623cfcb6aee5990e9064d00f2; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:8586071402183f841f60c659930f99cfd5789350 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:a480d751110e5476fefdf4ed0ec642e94d075dd7 && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:4ddf1377ce3077101d811ca831bdc75cc7db6e70 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:8c2a7c8d9a259d7623cfcb6aee5990e9064d00f2 && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
